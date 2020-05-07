161101dd33293e9ededced4264905edb12bfa0e6
mohan <mohanchaudhari9@gmail.com>
2020-05-07 15:20:35 +0530

    Update deprecated class

--- app/src/main/java/com/mohan/reddit/ui/main/MainFragment.kt
+++ app/src/main/java/com/mohan/reddit/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.mohan.reddit.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.mohan.reddit.R
 
 class MainFragment : Fragment() {
@@ -16,14 +16,16 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
